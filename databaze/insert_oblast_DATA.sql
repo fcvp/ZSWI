0cece93a9c5922c4f362ea989ec8da0fb097e0f0
Jan Samek <samcza@centrum.cz>
2015-04-20 11:30:19 +0200

    insert oblast

---
+++ databaze/insert_oblast_DATA.sql
@@ -0,0 +1,33 @@
+call insert_oblast('Matematika');
+
+call insert_oblast('Fyzika');
+
+call insert_oblast('Informatika');
+
+call insert_oblast('Počítačová grafika');
+
+call insert_oblast('Biologie');
+
+call insert_oblast('Chemie');
+
+call insert_oblast('Ekonomika');
+
+call insert_oblast('Mechanika');
+
+call insert_oblast('Výpočetní technika');
+
+call insert_oblast('Stavitelství');
+
+call insert_oblast('Jazyky');
+
+call insert_oblast('Kybernetika');
+
+call insert_oblast('Zeměpis');
+
+call insert_oblast('Elektrotechnika');
+
+call insert_oblast('Geometrie');
+
+call insert_oblast('Geomatika');
+
+call insert_oblast('Výpočetní technika');
